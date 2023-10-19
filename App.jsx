ced774b03ddf185792e23349f58b3ab029a63cb1
Gradity Mantiri <mantirigradity29@gmail.com>
2023-10-20 07:55:55 +0800

    menambahkan

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './molecules/header';
+import Home from './molecules/home';
+import Footer from './molecules/footer';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Home/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
